b4d6ce73fcfe7bd8b66d5bb01b23d3fcc2ab461c
Samantha Giles <144616302+ssamanthagiles@users.noreply.github.com>
2024-01-10 17:43:18 -0500

    Problem Set 1: C
    Introduction to C programming language, covering basics like variables, conditions, loops, and functions.

---
+++ cash.c
@@ -0,0 +1,96 @@
+
+#include <cs50.h>
+#include <stdio.h>
+
+int calculate_quarters(int cents);
+
+int main(void)
+
+{
+    // Prompt the user for change owed, in cents
+    int cents;
+    do
+    {
+        cents = get_int("Change owed: ");
+    }
+    while (cents < 0);
+
+    // Calculate how many quarters you should give customer
+    int quarters = calculate_quarters(cents);
+
+    // Subtract the value of those quarters from cents
+    cents = cents - (quarters * 25);
+
+    // Calculate how many dimes you should give customer
+    int calculate_dimes(int);
+    int dimes = calculate_dimes(cents);
+
+    // Subtract the value of those dimes from cents
+    cents = cents - (dimes * 10);
+
+    // Calculate how many nickels you should give customer
+    int calculate_nickels(int);
+    int nickels = calculate_nickels(cents);
+
+    // Subtract the value of those nickels from cents
+    cents = cents - (nickels * 5);
+
+    // Calculate how many pennies you should give customer
+    int calculate_pennies(int);
+    int pennies = calculate_pennies(cents);
+
+    // Subtract the value of those nickels from cents
+    cents = cents - (pennies * 1);
+    // Sum the number of quarters, dimes, nickels, and pennies used
+    int sum = (quarters + dimes + nickels + pennies);
+    // Print that sum
+    printf("%d", sum);
+}
+
+int calculate_quarters(int cents)
+{
+    // Calculate how many quarters you should give customer
+    int quarters = 0;
+    while (cents >= 25)
+    {
+        quarters++;
+        cents = cents - 25;
+    }
+    return quarters;
+}
+
+int calculate_dimes(int cents)
+{
+    // Calculate how many dimes you should give customer
+    int dimes = 0;
+    while (cents >= 10)
+    {
+        dimes++;
+        cents = cents - 10;
+    }
+    return dimes;
+}
+
+int calculate_nickels(int cents)
+{
+    // Calculate how many nickels you should give customer
+    int nickels = 0;
+    while (cents >= 5)
+    {
+        nickels++;
+        cents = cents - 5;
+    }
+    return nickels;
+}
+
+int calculate_pennies(int cents)
+{
+    // Calculate how many nickels you should give customer
+    int pennies = 0;
+    while (cents >= 1)
+    {
+        pennies++;
+        cents = cents - 1;
+    }
+    return pennies;
+}
